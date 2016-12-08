73be25a68fa6acf80bb56510435475cd37ce0424
dianagr95 <diana_gr95@hotmail.com>
2016-12-08 11:35:39 -0600

    primer commit

---
+++ Formulario2/app/src/main/java/com/example/diana/formulario2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.diana.formulario2;
+
+import android.os.Bundle;
+import android.app.
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
